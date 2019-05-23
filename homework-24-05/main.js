b711981ef7fca987d60cdb1d8d25886183fbec40
IrynaVynnychenko <i.vynnychenko@gmail.com>
2019-05-23 13:39:28 +0300

    push changes

--- homework-24-05/main.js
+++ homework-24-05/main.js
@@ -1,22 +1,25 @@
 let m;
-let string = 0;
-let number = 0;
+// let string = 0;
+// let number = 0;
+let string = [];
+let number = [];
 
-let MyArray = [1 / 5, 10 * 2 < 15, null, "wonder", m, ['a', 'b', 'c'], { today: "Lazy day" }, 23.6, ['кекс', 5, 'арбуз', 78]];
+let myarr = [1 / 5, 10 * 2 < 15, null, 'wonder', m, ['a', 'b', 'c'], { today: "Lazy day" }, 23.6, ['кекс', 5, 'арбуз', 78]];
 
-for (let i = 0; i < array.length; i++) {
-    if (typeof array[i] === "number") {
-        number.push(array[i]);
-    } else if (typeof array[i] === "string") {
-        str.push(array[i];
-        }
-        else if (Array.isArray(array[i])) {
-            for (let j = 0; j < array[i].length; j++) {
-                if (typeof array[i][j] === "number") {
-                    number.push(array[i];
-                    }
-                }
+for (let i = 0; i < myarr.length; i++) {
+    if (typeof myarr[i] === "number") {
+        number.push(myarr[i]);
+    } else if (typeof myarr[i] === "string") {
+        string.push(myarr[i]);
+    } else if (Array.isArray(myarr[i])) {
+        for (let j = 0; j < myarr[i].length; j++) {
+            if (typeof myarr[i][j] === "number") {
+                number.push(myarr[i][j]);
+            } else if (typeof myarr[i][j] === "string") {
+                string.push(myarr[i][j]);
             }
         }
+    }
+}
 
-        console.log(`str: ${str}\nnumber: ${number}`);
+console.log(`string: ${string}\nnumber: ${number}`);
